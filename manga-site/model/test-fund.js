9676cea981bfd5441f9af2a3ca3e93a9752107fa
tuantran89 <tuantran050289@gmail.com>
2022-02-28 21:18:07 +0700

    security

--- manga-site/model/test-fund.js
+++ manga-site/model/test-fund.js
@@ -1,20 +1,17 @@
 const Fund = require("./fund");
+var sanitize = require('mongo-sanitize');
 module.exports = {
     getFundData: function (req, res) {
         let fundId = req.query["fund_id"];
         let fundSource = req.query["fund_source"];
         let promise;
         if (fundSource === "manual") {
-            if (typeof fundId != "object") {
-                let condition = { fund_id: fundId };
-                if (ObjectId.isValid(fundId)) {
-                    condition = { _id: fundId };
-                }
-                promise = Fund.findOne(condition);
-            } else {
-                console.log('Not accept object type');
-                promise = Promise.reject('Not accept object type');
+            var cleanFundId = sanitize(fundId);
+            let condition = { fund_id: cleanFundId };
+            if (ObjectId.isValid(cleanFundId)) {
+                condition = { _id: cleanFundId };
             }
+            promise = Fund.findOne(condition);
         
         }
         return promise;
